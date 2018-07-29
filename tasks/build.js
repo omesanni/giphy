656d494e21dede3be1b4ef983909131cec136978
Omeyimi Sanni <omesanni12345@yahoo.com>
2018-07-29 12:05:46 -0400

    build file refactored

--- tasks/build.js
+++ tasks/build.js
@@ -6,12 +6,14 @@ const webpackConfig = require('../config/webpack.prod.config');
 
 if (process.env.NODE_ENV === 'production') {
   webpack(webpackConfig).run((err) => {
-    console.log('Started webpack...'); // eslint-disable-line
+    /* eslint-disable no-console */
+    console.log('Started webpack...');
 
     if (err) {
-      return console.error(err); // eslint-disable-line
+      return console.error(err);
     }
 
-    return console.log('Finished webpack...'); // eslint-disable-line
+    return console.log('Finished webpack...');
+    /* eslint-enable no-console */
   });
 }
